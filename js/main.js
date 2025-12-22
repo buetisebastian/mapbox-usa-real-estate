0d7da947be18a5b37f86c67dbae88ff254d09ebd
buetisebastian <115380421+buetisebastian@users.noreply.github.com>
2025-12-22 12:39:12 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'PEGA_ACA_TU_TOKEN';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v11',
+  center: [-98.5, 39.8],
+  zoom: 3
+});
+
+map.addControl(new mapboxgl.NavigationControl());
